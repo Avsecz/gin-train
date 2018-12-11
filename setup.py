d3a0fff3141b7f90c394b51e2703d1521d779a77
Avsecz <zigaavsec@gmail.com>
2018-12-11 01:54:55 +0100

    Bump version: 0.1.7 → 0.1.8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='gin-train',
-    version='0.1.7',
+    version='0.1.8',
     description="gin-train: model training boilerplate",
     author="Ziga Avsec",
     author_email='avsec@in.tum.de',
